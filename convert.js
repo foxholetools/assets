eccdb080b844fddcfde279dffff1ad1cd7ef4a77
samuelds <samuelds.pro@gmail.com>
2021-09-17 23:03:15 +0200

    Update sources

--- convert.js
+++ convert.js
@@ -29,7 +29,7 @@ async function run()
     console.log('-----[ Start convertion ]-----');;
     for await (directory of directorys)
     {
-		console.log("Start convert " + source);
+		console.log("Start convert " + directory.source);
         const sourcePath = sourcesDirectory + '/' + directory.source;
         const destPath = publicDirectory + '/' + directory.dest;
         await tgaToPng(sourcePath, destPath);
