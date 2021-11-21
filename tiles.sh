d2b8c3eb1e574fa0ef455465c0af1fae99b9c660
samuelds <samuelds.pro@gmail.com>
2021-11-21 12:19:49 +0100

    Update script

--- tiles.sh
+++ tiles.sh
@@ -2,23 +2,29 @@
 
 list="acrithia allodsbight ashfields basinsionnach callahanspassage clansheadvalley deadlands drownedvale endlessshore farranaccoast fishermansrow godcrofts greatmarch heartlands howlcounty kalokai linnmercy lochmor nevishline marbanhollow morgenscrossing mooringcounty oarbreaker origin reachingtrail redriver shackledchasm speakingwoods stonecradle tempestisland terminus thefingers umbralwildwood viperpit weatheredexpanse westgate homeregionc homeregionw"
 
+path=$PWD
+
 for hexagone in $list
 do
 
     echo --------------------
     echo   Start $hexagone
     echo --------------------
+	
 
     # Clasic
     echo - Clasic
+	
+	echo Clear old dir
+	rm -Rf $path/dist/tiles/clasic/$hexagone
 
-    echo Generatetiles
-    $PWD/libs/tiler/binaries/Tiler-Linux-amd64 -zoom 7 -input $PWD/dist/maps/clasic/$hexagone.png -output $PWD/dist/tiles/clasic/$hexagone
+    echo Generate tiles
+    $path/libs/tiler/binaries/Tiler-Linux-amd64 -zoom 7 -input $path/dist/maps/clasic/$hexagone.png -output $path/dist/tiles/clasic/$hexagone
 
-    cd $PWD/dist/tiles/clasic/$hexagone
+    cd $path/dist/tiles/clasic/$hexagone
 
     # create dirs
-    mkdir 0 1 2 3 4 5 6 7 8
+    mkdir 0 1 2 3 4 5 6 7
 
     # clean empty image
     #find . -size 1k -delete
@@ -32,12 +38,15 @@ do
     done
 
     # Color
-    echo - Color
+    echo - Color	
+	
+	echo Clear old dir
+	rm -Rf $path/dist/tiles/color/$hexagone
 
     echo Generate tiles
-    $PWD/libs/tiler/binaries/Tiler-Linux-amd64 -zoom 7 -input $PWD/dist/maps/color/$hexagone.png -output $PWD/dist/tiles/color/$hexagone
+    $path/libs/tiler/binaries/Tiler-Linux-amd64 -zoom 7 -input $path/dist/maps/color/$hexagone.png -output $path/dist/tiles/color/$hexagone
 
-    cd $PWD/dist/tiles/color/$hexagone
+    cd $path/dist/tiles/color/$hexagone
 
     # create dirs
     mkdir 0 1 2 3 4 5 6 7 8
