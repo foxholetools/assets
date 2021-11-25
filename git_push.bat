921923cca6e1c7d9a5824124fd9ac9fe6c2ac94e
samuelds <samuelds.pro@gmail.com>
2021-11-25 10:23:38 +0100

    Update

--- git_push.bat
+++ git_push.bat
@@ -2,18 +2,20 @@
 
 set list=acrithia allodsbight ashfields basinsionnach callahanspassage clansheadvalley deadlands drownedvale endlessshore farranaccoast fishermansrow godcrofts greatmarch heartlands howlcounty kalokai linnmercy lochmor nevishline marbanhollow morgenscrossing mooringcounty oarbreaker origin reachingtrail redriver shackledchasm speakingwoods stonecradle tempestisland terminus thefingers umbralwildwood viperpit weatheredexpanse westgate homeregionc homeregionw
 
-set path=%cd%
-
 for %%v in (%list%) do (
 
     echo Start %%v
-
+	
     :: Clasic
-    %path%\libs\tiler\binaries\Tiler-Windows-x64.exe -zoom 7 -input %path%\dist\maps\clasic\%%v.png -output %path%\dist\tiles\clasic\%%v
-
-    
+    git add %cd%\dist\tiles\clasic\%%v
 
     :: Color
-    %path%\libs\tiler\binaries\Tiler-Windows-x64.exe -zoom 7 -input %path%\dist\maps\color\%%v.png -output %path%\dist\tiles\color\%%v
+    git add %cd%\dist\tiles\color\%%v
+	
+	:: Commit and push
+	git commit -m "Update %%v"
+	git push
+	
+	pause
 
 )
